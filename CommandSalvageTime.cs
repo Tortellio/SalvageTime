b97163047139281f62452a1fee9ef45111fa9340
Irfan Miftah <irfan_miftah51@yahoo.com>
2020-05-03 13:29:02 +0700

    Initial Release

---
+++ CommandSalvageTime.cs
@@ -0,0 +1,40 @@
+﻿using Rocket.API;
+using Rocket.Unturned.Chat;
+using Rocket.Unturned.Player;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Tortellio.SalvageTimeChanger.Commands
+{
+    public class CommandSalvageTime : IRocketCommand
+    {
+        public string Name => "salvagetime";
+        public string Help => "Change your salvage time";
+        public string Syntax => "<seconds>";
+        public List<string> Aliases => new List<string>() { "stime" };
+        public List<string> Permissions => new List<string>() { "salvagetime" };
+        public AllowedCaller AllowedCaller => AllowedCaller.Player;
+        public void Execute(IRocketPlayer caller, string[] command)
+        {
+            if (caller == null) return;
+            UnturnedPlayer player = (UnturnedPlayer)caller;
+            
+            if (command.Length != 1)
+            {
+                UnturnedChat.Say(caller, SalvageTimeChanger.Instance.Translate("usage"), Color.green, "https://i.imgur.com/FeIvao9.png");
+                return;
+            }
+
+            if (!byte.TryParse(command[0], out byte result))
+            {
+                UnturnedChat.Say(caller, SalvageTimeChanger.Instance.Translate("invalid_time"), Color.green, "https://i.imgur.com/FeIvao9.png");
+                return;
+            }
+
+            if (result < 1) player.Player.interact.sendSalvageTimeOverride(1);
+            else player.Player.interact.sendSalvageTimeOverride(result);
+            UnturnedChat.Say(caller, SalvageTimeChanger.Instance.Translate("salvagetime_changed", result.ToString()), Color.green, "https://i.imgur.com/6EoLIWM.png");
+            return;
+        }
+    }
+}
---
+++ SalvageTimeChanger.cs
@@ -0,0 +1,42 @@
+﻿using Rocket.API;
+using Rocket.API.Collections;
+using Rocket.Core.Plugins;
+using Rocket.Unturned.Player;
+using System;
+using Steamworks;
+using SDG.Unturned;
+using Logger = Rocket.Core.Logging.Logger;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Tortellio.SalvageTimeChanger
+{
+    public class SalvageTimeChanger : RocketPlugin
+    {
+        public static SalvageTimeChanger Instance;
+        public static string PluginName = "SalvageTimeChanger";
+        public static string PluginVersion = " 1.0.0";
+
+        protected override void Load()
+        {
+            Instance = this;
+            Logger.Log("SalvageTimeChanger has been loaded!");
+            Logger.Log(PluginName + PluginVersion, ConsoleColor.Yellow);
+            Logger.Log("Made by Tortellio", ConsoleColor.Yellow);
+        }
+
+        protected override void Unload()
+        {
+            Instance = null;
+            Logger.Log("SalvageTimeChanger has been unloaded!");
+            Logger.Log("Visit Tortellio Discord for more! https://discord.gg/pzQwsew", ConsoleColor.Yellow);
+        }
+
+        public override TranslationList DefaultTranslations => new TranslationList()
+        {
+            { "invalid_time", "Invalid amount of time! Usage: /salvagetime <seconds>" },
+            { "usage", "Usage: /salvagetime <seconds>" },
+            { "salvagetime_changed", "Your salvage time has changed to {0} seconds!" },
+        };
+    }
+}
